cd70ad0b49ecab0ff4925a8fd585c8b1de6927cc
Yaroslav <mx.yarik@gmail.com>
2019-05-25 17:49:26 +0300

    Updates

---
+++ precache-manifest.de879145f5e3b1302f0675f949b5f709.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = [
+  {
+    "revision": "f225f5023844376e0a71",
+    "url": "/kanban-board/static/css/main.33184a3f.chunk.css"
+  },
+  {
+    "revision": "f225f5023844376e0a71",
+    "url": "/kanban-board/static/js/main.a513b2e6.chunk.js"
+  },
+  {
+    "revision": "2a97cbc7210d9a44b96f",
+    "url": "/kanban-board/static/js/runtime~main.8507f43b.js"
+  },
+  {
+    "revision": "4ad1b1bc32e565bb8377",
+    "url": "/kanban-board/static/js/2.15bd953c.chunk.js"
+  },
+  {
+    "revision": "db5bf935ecefe5a8208c276a20b79b90",
+    "url": "/kanban-board/static/media/delete_icon.db5bf935.svg"
+  },
+  {
+    "revision": "a58c7cc58cf0906f75137525c90be7f2",
+    "url": "/kanban-board/static/media/rubbish.a58c7cc5.svg"
+  },
+  {
+    "revision": "b1e00d6645c83083a1c1762ef6462c92",
+    "url": "/kanban-board/static/media/add_card_icon.b1e00d66.svg"
+  },
+  {
+    "revision": "9f8efea00f464d1d772669f773e33118",
+    "url": "/kanban-board/static/media/mlechnyy-put-noch-zvezdy-nebo.9f8efea0.jpg"
+  },
+  {
+    "revision": "7877090582a078cf7ea3b69eae355d35",
+    "url": "/kanban-board/static/media/Montserrat-Thin.78770905.woff"
+  },
+  {
+    "revision": "4fff33ac95aaed9f68e1471c805558ec",
+    "url": "/kanban-board/static/media/Montserrat-Light.4fff33ac.woff"
+  },
+  {
+    "revision": "3d2d13853665147fb8c2353d61c077ce",
+    "url": "/kanban-board/static/media/Montserrat-SemiBold.3d2d1385.woff"
+  },
+  {
+    "revision": "f555c75a92fbeac44c42c820f7e5101d",
+    "url": "/kanban-board/static/media/Montserrat-ExtraLight.f555c75a.woff"
+  },
+  {
+    "revision": "a79b68fcda60ce8f337f51d3220ef2a5",
+    "url": "/kanban-board/static/media/Montserrat-Bold.a79b68fc.woff"
+  },
+  {
+    "revision": "de4a911e831ca01751ab001a54de34dd",
+    "url": "/kanban-board/static/media/Montserrat-Regular.de4a911e.woff"
+  },
+  {
+    "revision": "d194e50992ca40d4752c6649f8684575",
+    "url": "/kanban-board/static/media/Montserrat-Medium.d194e509.woff"
+  },
+  {
+    "revision": "28eef764f61e16cec1a528eaba86f491",
+    "url": "/kanban-board/static/media/Montserrat-ExtraBold.28eef764.woff"
+  },
+  {
+    "revision": "ad732b72a16aa8d53dc9b44784aa305b",
+    "url": "/kanban-board/static/media/Montserrat-MediumItalic.ad732b72.woff"
+  },
+  {
+    "revision": "d743ecc0e682f6684325cf9441d8ca23",
+    "url": "/kanban-board/static/media/Montserrat-ExtraLightItalic.d743ecc0.woff"
+  },
+  {
+    "revision": "6fa635498a34d29852a176a684052fca",
+    "url": "/kanban-board/static/media/Montserrat-BoldItalic.6fa63549.woff"
+  },
+  {
+    "revision": "e406133e5d4239c989b5f25f7ed34d0d",
+    "url": "/kanban-board/static/media/Montserrat-ExtraBoldItalic.e406133e.woff"
+  },
+  {
+    "revision": "74f69ef5906c6a5b074573e7b65f3bbe",
+    "url": "/kanban-board/static/media/Montserrat-Italic.74f69ef5.woff"
+  },
+  {
+    "revision": "bc4d475cab39c1afc59fdbbed7419b7d",
+    "url": "/kanban-board/static/media/Montserrat-LightItalic.bc4d475c.woff"
+  },
+  {
+    "revision": "87d5d2706e231acd7d9871ed5c3b8bae",
+    "url": "/kanban-board/static/media/Montserrat-BlackItalic.87d5d270.woff"
+  },
+  {
+    "revision": "ac031a320dd5f7e047e69e8c631c8fd7",
+    "url": "/kanban-board/static/media/Montserrat-SemiBoldItalic.ac031a32.woff"
+  },
+  {
+    "revision": "179f8e8da7246abc5525d489e98f1971",
+    "url": "/kanban-board/static/media/Montserrat-ThinItalic.179f8e8d.woff"
+  },
+  {
+    "revision": "c20b9740ecf9beae3e6b0ff9b8e11549",
+    "url": "/kanban-board/static/media/Montserrat-Black.c20b9740.woff"
+  },
+  {
+    "revision": "ba044963492ff3ec7755d1117661a360",
+    "url": "/kanban-board/static/media/Montserrat-Light.ba044963.ttf"
+  },
+  {
+    "revision": "1de7f0493a20873dd93aea5625fb1b40",
+    "url": "/kanban-board/static/media/Montserrat-Light.1de7f049.eot"
+  },
+  {
+    "revision": "143850af7ce7bad6c8012715f25c10f9",
+    "url": "/kanban-board/static/media/Montserrat-Thin.143850af.eot"
+  },
+  {
+    "revision": "2597559f3f979e5d8bd947eac5a94af7",
+    "url": "/kanban-board/static/media/Montserrat-Thin.2597559f.ttf"
+  },
+  {
+    "revision": "117eff1ff962782e3e38288d21399c03",
+    "url": "/kanban-board/static/media/Montserrat-ExtraLight.117eff1f.eot"
+  },
+  {
+    "revision": "490fa183366c13649dfa99ab55c583cc",
+    "url": "/kanban-board/static/media/Montserrat-ExtraLight.490fa183.ttf"
+  },
+  {
+    "revision": "f7e77a493f9ba88c23ac9b85609fafc4",
+    "url": "/kanban-board/static/media/Montserrat-Medium.f7e77a49.eot"
+  },
+  {
+    "revision": "41940db30d513f31c9adbf15f4a98a76",
+    "url": "/kanban-board/static/media/Montserrat-Medium.41940db3.ttf"
+  },
+  {
+    "revision": "b3ddbb16a2b82841b1f70a88173cef56",
+    "url": "/kanban-board/static/media/Montserrat-MediumItalic.b3ddbb16.eot"
+  },
+  {
+    "revision": "45014625eaedf18b1b5fbe63caa80064",
+    "url": "/kanban-board/static/media/Montserrat-BoldItalic.45014625.ttf"
+  },
+  {
+    "revision": "5c7f99074234e7c7db89a588614fe9b9",
+    "url": "/kanban-board/static/media/Montserrat-MediumItalic.5c7f9907.ttf"
+  },
+  {
+    "revision": "b8f5b73d80a81dd55cad82f683c343be",
+    "url": "/kanban-board/static/media/Montserrat-BoldItalic.b8f5b73d.eot"
+  },
+  {
+    "revision": "43d12d16172b519f448064bcd73d579c",
+    "url": "/kanban-board/static/media/Montserrat-ExtraLightItalic.43d12d16.eot"
+  },
+  {
+    "revision": "c139ffb2e2744925acd3c871f2c96ac0",
+    "url": "/kanban-board/static/media/Montserrat-SemiBold.c139ffb2.eot"
+  },
+  {
+    "revision": "cf9a7578398f9cc84d010a52bcc50d6b",
+    "url": "/kanban-board/static/media/Montserrat-ExtraLightItalic.cf9a7578.ttf"
+  },
+  {
+    "revision": "aebf4fe19e37e3d508a8f6950f9378d1",
+    "url": "/kanban-board/static/media/Montserrat-SemiBold.aebf4fe1.ttf"
+  },
+  {
+    "revision": "c47cfa55a714f756f49f6c74b9da14ec",
+    "url": "/kanban-board/static/media/Montserrat-Italic.c47cfa55.ttf"
+  },
+  {
+    "revision": "bf2c26d2b14e3fc3b9f065fb308866c8",
+    "url": "/kanban-board/static/media/Montserrat-Bold.bf2c26d2.ttf"
+  },
+  {
+    "revision": "92f84d8b5d51482fcc4ea10c6361b7fa",
+    "url": "/kanban-board/static/media/Montserrat-Italic.92f84d8b.eot"
+  },
+  {
+    "revision": "428f1389af3038385bb834208ea5043b",
+    "url": "/kanban-board/static/media/Montserrat-ExtraBoldItalic.428f1389.ttf"
+  },
+  {
+    "revision": "2e1e2b598ca1d1be767fcbb2d5a9dcd5",
+    "url": "/kanban-board/static/media/Montserrat-LightItalic.2e1e2b59.eot"
+  },
+  {
+    "revision": "5ee3b1a7b2520405642ec80d1fef2c30",
+    "url": "/kanban-board/static/media/Montserrat-ExtraBoldItalic.5ee3b1a7.eot"
+  },
+  {
+    "revision": "dadd3c3bf4846aa66cf350a26b253f50",
+    "url": "/kanban-board/static/media/Montserrat-LightItalic.dadd3c3b.ttf"
+  },
+  {
+    "revision": "a361f0ccb74ec4738fc9cf06d4f8883c",
+    "url": "/kanban-board/static/media/Montserrat-Bold.a361f0cc.eot"
+  },
+  {
+    "revision": "74fbdb736c8c84bb98735510d092d887",
+    "url": "/kanban-board/static/media/Montserrat-SemiBoldItalic.74fbdb73.ttf"
+  },
+  {
+    "revision": "70008e3309cd3c1ce362d8c8547dbbc5",
+    "url": "/kanban-board/static/media/Montserrat-SemiBoldItalic.70008e33.eot"
+  },
+  {
+    "revision": "2dd0a1de870af34d48d43b7cad82b8d9",
+    "url": "/kanban-board/static/media/Montserrat-Regular.2dd0a1de.eot"
+  },
+  {
+    "revision": "08a754d1d68f4671eab5e11ece77cd03",
+    "url": "/kanban-board/static/media/Montserrat-Regular.08a754d1.ttf"
+  },
+  {
+    "revision": "21edc00be8e452af37ebcd2073178cb0",
+    "url": "/kanban-board/static/media/Montserrat-ExtraBold.21edc00b.eot"
+  },
+  {
+    "revision": "2a3d13e3cafb3f088e5a4b3c9ff435cb",
+    "url": "/kanban-board/static/media/Montserrat-ExtraBold.2a3d13e3.ttf"
+  },
+  {
+    "revision": "4817b66e5f364cab8b47e70c526fd87f",
+    "url": "/kanban-board/static/media/Montserrat-ThinItalic.4817b66e.eot"
+  },
+  {
+    "revision": "5cd51f31feb03cd98e765f62ceef9fb3",
+    "url": "/kanban-board/static/media/Montserrat-ThinItalic.5cd51f31.ttf"
+  },
+  {
+    "revision": "f2e252596cbdb9d2c80256968feff8cd",
+    "url": "/kanban-board/static/media/Montserrat-Black.f2e25259.eot"
+  },
+  {
+    "revision": "594626363a3b93b3fe22e039e66448c4",
+    "url": "/kanban-board/static/media/Montserrat-Black.59462636.ttf"
+  },
+  {
+    "revision": "e70885e2d19798607dffd52a398d8a89",
+    "url": "/kanban-board/static/media/Montserrat-BlackItalic.e70885e2.eot"
+  },
+  {
+    "revision": "f9d1621e8aaf9b26d3d26092e7a7a632",
+    "url": "/kanban-board/static/media/Montserrat-BlackItalic.f9d1621e.ttf"
+  },
+  {
+    "revision": "bd8d1f585309c6241ce0d6d7239ee737",
+    "url": "/kanban-board/index.html"
+  }
+];
